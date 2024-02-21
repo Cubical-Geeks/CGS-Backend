296799b4f370b0597da95844d7c6bb3cf5444b68
Cubical-Geeks <158097745+Cubical-Geeks@users.noreply.github.com>
2024-02-21 21:59:27 +0500

    update

--- src/Helper/cloudinary.js
+++ src/Helper/cloudinary.js
@@ -1,5 +1,6 @@
-import { v2 as cloudinary } from "cloudinary";
-import fs from "fs";
+// import { v2 as cloudinary } from "cloudinary";
+// import fs from "fs";
+const cloudinary = require("cloudinary").v2;
 
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
@@ -22,4 +23,5 @@ const uploadOnCloudinary = async (localFilePath) => {
   }
 };
 
-export { uploadOnCloudinary };
+// export { uploadOnCloudinary };
+module.exports = { uploadOnCloudinary };
